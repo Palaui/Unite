2c5a43102c7badfd632402e2e1ed16591944bd19
Palaui <sucdepressec@gmail.com>
2017-09-18 17:48:52 +0200

    Added Button utilities

---
+++ Unite/Components/Utilities/AdaptButtonPress.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Unite
+{
+    class AdaptButtonPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        // Variables
+        #region Variables
+
+        public Outline outline;
+        public Color outlineColor = Color.white;
+        public Image image;
+        public Color imageColor = Color.white;
+        public Text text;
+        public Color textColor = Color.white;
+
+        private Button button;
+        private Color outlineBaseColor;
+        private Color imageBaseColor;
+        private Color textBaseColor;
+
+        private bool isValid;
+
+        #endregion
+
+        // Override
+        #region Override
+
+        void Start()
+        {
+            button = GetComponent<Button>();
+            if (button)
+            {
+                if (outline)
+                    outlineBaseColor = outline.effectColor;
+                if (image)
+                    imageBaseColor = image.color;
+                if (text)
+                    textBaseColor = text.color;
+                isValid = true;
+            }
+            else
+                enabled = false;
+        }
+
+        #endregion
+
+        // Events
+        #region Events
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (isValid)
+            {
+                if (outline)
+                    outline.effectColor = outlineColor;
+                if (image)
+                    image.color = imageColor;
+                if (text)
+                    text.color = textColor;
+            }
+
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (isValid)
+            {
+                if (outline)
+                    outline.effectColor = outlineBaseColor;
+                if (image)
+                    image.color = imageBaseColor;
+                if (text)
+                    text.color = textBaseColor;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Unite/Components/Utilities/InitialDeactivate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Unite
+{
+    class InitialDeactivate : MonoBehaviour
+    {
+        // Variables
+        #region Variables
+
+        public bool deactivateImmediate = false;
+
+        #endregion
+
+        // Override
+        #region Override
+
+        void Awake()
+        {
+            if (deactivateImmediate)
+                gameObject.SetActive(false);
+        }
+
+        void Start()
+        {
+            if (Time.fixedUnscaledTime < 1 && !deactivateImmediate)
+                StartCoroutine(Deactivate());
+        }
+
+        #endregion
+
+        // Evemts
+        #region Evemts
+
+        private IEnumerator Deactivate()
+        {
+            yield return new WaitForSeconds(0.6f - GetComponentsInParent<InitialDeactivate>(true).Length * 0.1f);
+            gameObject.SetActive(false);
+        }
+
+        #endregion
+    }
+}
