dc1e61f40341905de4044c22395170f62ac587ac
Palaui <sucdepressec@gmail.com>
2017-09-27 18:26:25 +0200

    Neural Network Added

---
+++ Unite/Math/Sigmoid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Unite
+{
+    public class Sigmoid
+    {
+        public static double Output(double x)
+        {
+            if (x < -45.0)
+                return 0;
+            if (x > 45.0)
+                return 1;
+
+            return 1.0 / (1.0 + Mathf.Exp((float)-x));
+        }
+
+        public static double Derivative(double x)
+        {
+            return x * (1 - x);
+        }
+    }
+}
---
+++ Unite/Neural/Neuron.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Unite.Neural
+{
+	public class Neuron 
+	{
+        // Variables
+        #region Variables
+
+        public List<NeuralSynapse> InputSynapses { get; set; }
+		public List<NeuralSynapse> OutputSynapses { get; set; }
+		public double Bias { get; set; }
+		public double BiasDelta { get; set; }
+		public double Gradient { get; set; }
+		public double Value { get; set; }
+
+        #endregion
+
+        // Override
+        #region Override
+
+        public Neuron()
+        {
+            InputSynapses = new List<NeuralSynapse>();
+            OutputSynapses = new List<NeuralSynapse>();
+            Bias = NeuralNetwork.GetRandom();
+        }
+
+        public Neuron(IEnumerable<Neuron> inputNeurons) : this()
+		{
+			foreach (var inputNeuron in inputNeurons)
+			{
+				var synapse = new NeuralSynapse(inputNeuron, this);
+				inputNeuron.OutputSynapses.Add(synapse);
+				InputSynapses.Add(synapse);
+			}
+		}
+
+        #endregion
+
+        // Public
+        #region Public
+
+        public virtual double CalculateValue()
+		{
+			return Value = Sigmoid.Output(InputSynapses.Sum(a => a.Weight * a.InputNeuron.Value) + Bias);
+		}
+
+		public double CalculateError(double target)
+		{
+			return target - Value;
+		}
+
+		public double CalculateGradient(double? target = null)
+		{
+			if(target == null)
+				return Gradient = OutputSynapses.Sum(a => a.OutputNeuron.Gradient * a.Weight) * Sigmoid.Derivative(Value);
+
+			return Gradient = CalculateError(target.Value) * Sigmoid.Derivative(Value);
+		}
+
+		public void UpdateWeights(double learnRate, double momentum)
+		{
+			var prevDelta = BiasDelta;
+			BiasDelta = learnRate * Gradient;
+			Bias += BiasDelta + momentum * prevDelta;
+
+			foreach (var synapse in InputSynapses)
+			{
+				prevDelta = synapse.WeightDelta;
+				synapse.WeightDelta = learnRate * Gradient * synapse.InputNeuron.Value;
+				synapse.Weight += synapse.WeightDelta + momentum * prevDelta;
+			}
+		}
+
+        #endregion
+
+    }
+}
